0d494d1e7b6cc15b6c9ee20d4d08e1ed1264b692
ClickBox-sudo <60176352+ClickBox-sudo@users.noreply.github.com>
2020-01-26 18:23:28 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    function menu() {
+        if ($(".menu #bars").css('display') != 'none') {
+            $(".fa-bars").on('click', () => {
+                if ($('.menu-list').css('display') === 'flex') {
+                    $('.menu-list').css('display', 'none')
+                } else {
+                    $('.menu-list').css('display', 'flex')
+                }
+            });
+            $(".menu-list a").on('click', () => {
+                if ($('.menu-list').css('display') === 'flex') {
+                    $('.menu-list').css('display', 'none')
+                } else {
+                    $('.menu-list').css('display', 'flex')
+                }
+            })
+        };
+    }
+    menu();
+
+    function toggleServiceList() {
+        $(".service-list-button").click(function () {
+            $(".service-item").slideToggle(1000, 'linear');
+        });
+    }
+    toggleServiceList();
+
+
+
+    var service = $('.service-link');
+    $(service).on('click', (event) => {
+        var targetID = $(event.target).text().toLowerCase();
+        var sectionListInMain = $('.main section');
+        for (var i = 0; i < sectionListInMain.length; i++) {
+            var match = $(sectionListInMain[i]).attr('id');
+            console.log('#'+match)
+            match == targetID ?  $('#'+match).css({'display':'block'}): $('#'+match).css({'display':'none'})
+        }
+        
+    });
+});
+
+
+
